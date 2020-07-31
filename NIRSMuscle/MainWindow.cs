2f9b0534f2740de58bf509ee381b15515ca24cc0
Theodore Huppert <huppert1@pitt.edu>
2020-07-31 13:07:25 -0400

    initial commit

---
+++ NIRSMuscle/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gtk;
+using System.Net.Sockets;
+using System.Text;
+
+public partial class MainWindow : Gtk.Window
+{
+    UdpClient udpClient;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        udpClient = new UdpClient(2390);
+        udpClient.Connect("10.0.0.1",2390);
+
+
+     //   Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+     //   udpClient.Send(sendBytes, sendBytes.Length);
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
